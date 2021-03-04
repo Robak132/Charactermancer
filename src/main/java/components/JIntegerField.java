edce6ce8ee2a89c14068d2544ed6503f9d9fba08
Jakub Robaczewski <j.robaczewski@stud.elka.pw.edu.pl>
2021-03-04 13:26:43 +0100

    Components changes

--- src/main/java/components/JIntegerField.java
+++ src/main/java/components/JIntegerField.java
@@ -4,24 +4,28 @@ import javax.swing.*;
 
 public class JIntegerField extends JTypeField<Integer> {
     public JIntegerField() {
-        super();
+        super("%d");
         this.value = 0;
     }
     public JIntegerField(int value) {
-        super(value);
-        this.value = value;
+        super("%d");
+        setValue(value);
+    }
+    public JIntegerField(int value, String format) {
+        super(format);
+        setValue(value);
     }
 
     public void decrement(int number) {
         this.value-=number;
-        this.setText("" + value);
+        this.setText(String.format(format, value));
     }
     public void decrement() {
         decrement(1);
     }
     public void increment(int number) {
         this.value+=number;
-        this.setText("" + value);
+        this.setText(String.format(format, value));
     }
     public void increment() {
         increment(1);
@@ -29,20 +33,30 @@ public class JIntegerField extends JTypeField<Integer> {
 }
 class JTypeField<T> extends JTextField {
     T value;
+    String format;
 
-    public JTypeField() {
+    public JTypeField(String format) {
         super();
+        this.format = format;
     }
-    public JTypeField(T value) {
-        super(null, "" + value, 0);
+    public JTypeField(T value, String format) {
+        this(format);
         this.value = value;
     }
 
     public void setValue(T value) {
-        this.setText("" + value);
+        this.setText(String.format(format, value));
         this.value = value;
     }
     public T getValue() {
         return value;
+    }
+
+    public String getFormat() {
+        return format;
+    }
+    public void setFormat(String format) {
+        this.format = format;
+        this.setText(String.format(format, value));
     }
 }
