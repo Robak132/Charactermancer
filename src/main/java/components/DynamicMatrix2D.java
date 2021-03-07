381df37455e65b5b375d9ccb3870b2408155c67f
Jakub Robaczewski <j.robaczewski@stud.elka.pw.edu.pl>
2021-03-07 14:27:23 +0100

    GridPanel

---
+++ src/main/java/components/DynamicMatrix2D.java
@@ -0,0 +1,30 @@
+package components;
+
+public class DynamicMatrix2D<T> {
+    private T[][] matrix = (T[][]) new Object[1][1];
+
+    public void set(int x, int y, T value) {
+        if (x >= matrix.length) {
+            T[][] tmp = matrix;
+            matrix = (T[][]) new Object[x + 1][];
+            System.arraycopy(tmp, 0, matrix, 0, tmp.length);
+            for (int i = x; i < x + 1; i++) {
+                matrix[i] = (T[]) new Object[y];
+            }
+        }
+
+        if (y >= matrix[x].length) {
+            T[] tmp = matrix[x];
+            matrix[x] = (T[]) new Object[y + 1];
+            System.arraycopy(tmp, 0, matrix[x], 0, tmp.length);
+        }
+
+        matrix[x][y] = value;
+    }
+    public T get(int x, int y) {
+        return x >= matrix.length || y >= matrix[x].length ? null : matrix[x][y];
+    }
+    public T[] get(int x) {
+        return x >= matrix.length ? null : matrix[x];
+    }
+}
---
+++ src/main/java/components/GridPanel.java
@@ -0,0 +1,77 @@
+package components;
+import com.intellij.uiDesigner.core.GridConstraints;
+import com.intellij.uiDesigner.core.GridLayoutManager;
+import com.intellij.uiDesigner.core.Spacer;
+import org.hibernate.event.spi.DirtyCheckEvent;
+
+import javax.swing.*;
+import java.awt.*;
+import java.util.*;
+
+public class GridPanel extends JPanel {
+    private int columns=1;
+    private int rows=1;
+    private final DynamicMatrix2D<Component> components = new DynamicMatrix2D<>();
+    private final Map<Component, GridConstraints> items = new HashMap<>();
+
+    public static final int ALIGNMENT_HORIZONTAL = 0;
+    public static final int ALIGNMENT_NOTOP = 1;
+    public static final int ALIGNMENT_CENTER = 2;
+
+    public GridPanel() {
+        super();
+    }
+
+    public void addAuto(Component comp, Object constraints, boolean buildNow) {
+        GridConstraints editConstr = (GridConstraints) constraints;
+        int columnCount = editConstr.getColumn();
+        editConstr.setColumn(columnCount+1);
+        if (columns < columnCount+1) {
+            columns = columnCount+1;
+        }
+        int rowCount = editConstr.getRow();
+        editConstr.setRow(rowCount+1);
+        if (rows < rowCount+1) {
+            rows = rowCount+1;
+        }
+        items.put(comp, editConstr);
+        components.set(rowCount, columnCount, comp);
+
+        if (buildNow) {
+            build();
+        }
+    }
+    public void addAuto(Component comp, Object constraints) {
+        addAuto(comp, constraints, false);
+    }
+
+    public void build() {
+        build(GridPanel.ALIGNMENT_NOTOP);
+    }
+    public void build(int alignment) {
+        setLayout(new GridLayoutManager(rows+2, columns+2, new Insets(0, 0, 0, 0), -1, -1));
+        addSpacers(alignment);
+        for (Map.Entry<Component, GridConstraints> pair : items.entrySet()) {
+            super.add(pair.getKey(), pair.getValue());
+        }
+    }
+
+    public Component getComponent(int col, int row) {
+        return components.get(row, col);
+    }
+    public Component[] getComponents(int row) {
+        return components.get(row);
+    }
+
+    private void addSpacers(int alignment) {
+        super.add(new Spacer(), new GridConstraints(0, 0, rows, 1, GridConstraints.ANCHOR_CENTER, GridConstraints.FILL_HORIZONTAL, GridConstraints.SIZEPOLICY_WANT_GROW, GridConstraints.SIZEPOLICY_FIXED, null, null, null));
+        super.add(new Spacer(), new GridConstraints(0, columns+1, rows, 1, GridConstraints.ANCHOR_CENTER, GridConstraints.FILL_HORIZONTAL, GridConstraints.SIZEPOLICY_WANT_GROW, GridConstraints.SIZEPOLICY_FIXED, null, null, null));
+
+        if (alignment > 0) {
+            super.add(new Spacer(), new GridConstraints(rows+1, 0, 1, columns+2, GridConstraints.ANCHOR_CENTER, GridConstraints.FILL_VERTICAL, GridConstraints.SIZEPOLICY_FIXED, GridConstraints.SIZEPOLICY_WANT_GROW, null, null, null));
+        }
+        if (alignment == 2) {
+            super.add(new Spacer(), new GridConstraints(0, 0, 1, columns+2, GridConstraints.ANCHOR_CENTER, GridConstraints.FILL_VERTICAL, GridConstraints.SIZEPOLICY_FIXED, GridConstraints.SIZEPOLICY_WANT_GROW, null, null, null));
+        }
+    }
+}
